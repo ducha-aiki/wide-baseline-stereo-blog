8e8cdd556b2ddce1b5f5cf24932ed3945482669a
Mgr. Dmytro Mishkin <ducha.aiki@gmail.com>
2020-03-27 22:12:59 +0100

    push2

--- _action_files/action_entrypoint.sh
+++ _action_files/action_entrypoint.sh
@@ -13,7 +13,7 @@ eval "$(ssh-agent -s)"
 # notebook converter using nbdev
 cp /fastpages/settings.ini .
 pip install jupyter_latex_envs --user
-pip install git+https://github.com/ducha-aiki/nbdev@latex_envs_citationsc --user
+pip install git+https://github.com/ducha-aiki/nbdev@latex_envs_citations --user
 python /fastpages/nb2post.py
 
 
